df914eb32acfb75286c72ff1bc81182c0e6cb0ea
arish-mhrzj <arish.mhrzn2004@gmail.com>
2024-08-30 00:16:30 +0545

    insertion sort done

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -3,18 +3,21 @@ function insertion() {
   for (var i = 1; i < array_size; i++) {
     var key = div_sizes[i];
     var j = i - 1;
+    div_update(divs[i], div_sizes[i], "red");
     while (j >= 0 && div_sizes[j] > key) {
       div_update(divs[j + 1], div_sizes[j + 1], "red");
       div_update(divs[j], div_sizes[j], "red");
       div_sizes[j + 1] = div_sizes[j];
-      div_update(divs[j + 1], div_sizes[j + 1], "red");
-      div_update(divs[j], div_sizes[j], "red");
-      div_update(divs[j + 1], div_sizes[j + 1], "blue");
+      //   div_update(divs[j + 1], div_sizes[j + 1], "red");
+      //   div_update(divs[j], div_sizes[j], "red");
+      div_update(divs[j + 1], div_sizes[j + 1], "green");
       div_update(divs[j], div_sizes[j], "blue");
-      j -= 1;
+      console.log(j);
+      j = j - 1;
+      console.log(j);
     }
     div_sizes[j + 1] = key;
-    for (var t = 0; t < j; t++) {
+    for (var t = 0; t <= i; t++) {
       div_update(divs[t], div_sizes[t], "green");
     }
   }
